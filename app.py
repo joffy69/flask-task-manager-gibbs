42d5df00a9886e535cf956195add88b526859a74
joffy69 <jcwgibbs@gmail.com>
2020-11-05 21:36:48 +0000

    connection and debug/testing

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 if os.path.exists("env.py"):
     import env
